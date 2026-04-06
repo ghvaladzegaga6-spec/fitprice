9d4b864585e592e6ef78be3c67435a7f8a12ad9f
gaga <ghvaladzegaga6@gmail.com>
2026-04-06 15:35:54 +0400

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -73,8 +73,8 @@ const authLimiter = rateLimit({
 });
 
 app.use(globalLimiter);
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Routes
 app.use('/api/auth',            authLimiter, authRouter);
